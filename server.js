523a879f6938a2960bf667a2b18691fb680e84b8
catalinciornei047 <117486124+catalinciornei047@users.noreply.github.com>
2024-01-25 20:49:44 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,40 +9,20 @@ const io = require("socket.io")(server, {
 	}
 })
 
-const activeCalls = {}; // A map to store active calls by ID
-
 io.on("connection", (socket) => {
-    socket.emit("me", socket.id);
+	socket.emit("me", socket.id)
 
-    socket.on("disconnect", () => {
-        // Handle disconnection and remove from active calls
-        for (const callId in activeCalls) {
-            if (activeCalls[callId].includes(socket.id)) {
-                activeCalls[callId] = activeCalls[callId].filter(id => id !== socket.id);
-                if (activeCalls[callId].length === 0) {
-                    delete activeCalls[callId];
-                }
-                break;
-            }
-        }
-        socket.broadcast.emit("callEnded");
-    });
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	})
 
-    socket.on("callUser", (data) => {
-        // Update the logic to handle multiple users per call ID
-        if (!activeCalls[data.userToCall]) {
-            activeCalls[data.userToCall] = [];
-        }
-        activeCalls[data.userToCall].push(socket.id);
+	socket.on("callUser", (data) => {
+		io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+	})
 
-        for (const id of activeCalls[data.userToCall]) {
-            io.to(id).emit("callUser", { signal: data.signalData, from: data.from, name: data.name });
-        }
-    });
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	})
+})
 
-    socket.on("answerCall", (data) => {
-        io.to(data.to).emit("callAccepted", data.signal);
-    });
-});
-
-server.listen(5000, () => console.log("Server is running on port 5000"));
+server.listen(5000, () => console.log("server is running on port 5000"))
